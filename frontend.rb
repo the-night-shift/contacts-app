fedd4217fa5398595795ae7948593df8b83a76ad
Brian <riceb53@gmail.com>
2018-03-06 18:23:36 -0600

    make frontend.rb

--- frontend.rb
+++ frontend.rb
@@ -1,3 +1,27 @@
 require 'unirest'
 
-response = Unirest.get("http://localhost:3000/the-contact")
+# one contact
+# response = Unirest.get("http://localhost:3000/the-contact")
+
+# contact = response.body
+# p 'here is the contact:'
+# p "first name: #{contact['first_name']}"
+# p "last name: #{contact['last_name']}"
+# p "email: #{contact['email']}"
+# p "phone number: #{contact['phone_number']}"
+
+
+# all contacts
+response = Unirest.get("http://localhost:3000/all-contacts")
+contacts = response.body
+
+# loop through contacts, print out each particular contact
+
+contacts.each do |contact|
+  p 'here is one of the contacts:'
+  p "first name: #{contact['first_name']}"
+  p "last name: #{contact['last_name']}"
+  p "email: #{contact['email']}"
+  p "phone number: #{contact['phone_number']}"
+  p "-" * 30
+end
