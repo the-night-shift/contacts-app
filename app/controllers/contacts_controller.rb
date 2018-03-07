0dc7f0a2ad8ee163b2d2d57e7cb59efa4b686f82
Brian <riceb53@gmail.com>
2018-03-06 18:52:08 -0600

    add .as_json method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,21 +1,21 @@
 class ContactsController < ApplicationController
   def my_contact
     contact1 = Contact.first
-    render json: {first_name: contact1.first_name, last_name: contact1.last_name, phone_number: contact1.phone_number, email: contact1.email}
+    render json: contact1.as_json
   end
 
   def all_contacts
     # all contacts
     contacts = Contact.all
     # make an empty array
-    contacts_hashes = []
+    # contacts_hashes = []
     # loop through all my contacts
-    contacts.each do |contact|
-      # make a hash for each contact
-      the_contact_hash = {first_name: contact.first_name, last_name: contact.last_name, phone_number: contact.phone_number, email: contact.email}
-      # shovel that hash into the array
-      contacts_hashes << the_contact_hash
-    end
-    render json: contacts_hashes
+    # contacts.each do |contact|
+    #   # make a hash for each contact
+    #   the_contact_hash = {first_name: contact.first_name, last_name: contact.last_name, phone_number: contact.phone_number, email: contact.email}
+    #   # shovel that hash into the array
+    #   contacts_hashes << the_contact_hash
+    # end
+    render json: contacts.as_json
   end
 end
